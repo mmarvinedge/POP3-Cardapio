d6de55783a6490ab5c67764a89ba9de584470bcc
Márvin Edge <marvinedge@live.com>
2020-06-03 16:05:10 -0300

    Melhoria de exibição de sinopse dos produtos

--- src/main/java/com/popsales/model/FlavorPizza.java
+++ src/main/java/com/popsales/model/FlavorPizza.java
@@ -39,26 +39,28 @@ public class FlavorPizza {
         try {
             if (flavor != null) {
                 if (i.getProduct().getPromo()) {
-                    if (description != null) {
-                        StringBuilder sb = new StringBuilder();
-                        sb.append("<span style='display: inline-table !important; max-width: 100%; width: auto;' class='description-pizza'><strong>" + flavor + "</strong></div> <div class='description-value' style='display: inline-block !important;'><small><s>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</s></small> <span class='color-value'>" + OUtils.formatarMoeda(i.getProduct().getPrice().doubleValue()) + "</span></span>");
-                        sb.append("</div><br/><div style='float: right;'><i style='font-size: 12px; color: gray; font-weight: 400;'>" + description + "</i></div>");
-                        sb.append("<hr/>");
-                        return sb.toString();
-                    } else {
-                        return "<span class='description-pizza'><strong>" + flavor + "</strong></div> <div class='description-value'><small><s>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</s></small> <span class='color-value'>" + OUtils.formatarMoeda(i.getProduct().getPrice().doubleValue()) + "</span></span>";
-                    }
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("<div class='row' style='margin-top: 7px !important;'>"
+                            + nome(i) + valor(i)
+                            + "</div> "
+                            + "<div class='row'>"
+                            + descricao(i)
+                            + "</div>");
+                    return sb.toString();
                 } else {
-                    if (description != null) {
-                        StringBuilder sb = new StringBuilder();
-                        sb.append("<span style='display: inline-table !important; max-width: 100%; width: auto;' class='description-pizza'><strong>" + flavor + "</strong></div> <div class='description-value' style='display: inline-block !important;'><small><s>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</s></small> <span class='color-value'>" + OUtils.formatarMoeda(i.getProduct().getPrice().doubleValue()) + "</span></span>");
-                        sb.append("</div><br/><div style='float: right;'><i style='font-size: 12px; color: gray; font-weight: 400;'>" + description + "</i></div>");
-                        sb.append("<hr/>");
-                        return sb.toString();
-                        //return "<span style='display: inline-table !important;' class='description-pizza'><strong>" + flavor + "</strong></div> <div class='description-value' style='display: inline-block !important;'><span class='color-value'>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</span></span>";
-                    } else {
-                        return "<span class='description-pizza'><strong>" + flavor + "</strong></div> <div class='description-value'><span class='color-value'>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</span></span>";
-                    }
+                    StringBuilder sb = new StringBuilder();
+                    //sb.append("<span style='display: inline-table !important; max-width: 100%; width: auto;' class='description-pizza'><strong>" + flavor + "</strong></div> <div class='description-value' style='display: inline-block !important;'><small><s>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</s></small> <span class='color-value'>" + OUtils.formatarMoeda(i.getProduct().getPrice().doubleValue()) + "</span></span>");
+                    //sb.append("</div><br/><div style='float: right;'><i style='font-size: 12px; color: gray; font-weight: 400;'>" + description + "</i></div>");
+                    //sb.append("<hr/>");
+                    sb.append("<div class='row'>");
+                    sb.append(nome(i));
+                    sb.append(valor(i));
+                    sb.append("</div> ");
+                    sb.append("<div class='row'>");
+                    sb.append(descricao(i));
+                    sb.append("</div>");
+                    return sb.toString();
+                    //return "<span style='display: inline-table !important;' class='description-pizza'><strong>" + flavor + "</strong></div> <div class='description-value' style='display: inline-block !important;'><span class='color-value'>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</span></span>";
                 }
             } else {
                 return "";
@@ -68,10 +70,51 @@ public class FlavorPizza {
         }
     }
     
-    public String getHtmlDescription(Item i){
+    public String htmlGetFlavorDesktop(Item i) {
         try {
-            if(flavor != null){
-                if(description != null){
+            if (flavor != null) {
+                if (i.getProduct().getPromo()) {
+                    return "<span class='description-pizza'><strong>" + flavor + "</strong></div>  <div class='description-value'><small><s>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</s></small> <span class='color-value'>" + OUtils.formatarMoeda(i.getProduct().getPrice().doubleValue()) + "</span></span>";
+                } else {
+                    return "<span class='description-pizza'><strong>" + flavor + "</strong></div>  <div class='description-value'><span class='color-value'>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</span></span>";
+                }
+            } else {
+                return "";
+            }
+        } catch (Exception e) {
+            return flavor;
+        }
+    }
+
+    public String nome(Item i) {
+        if (i.getProduct().getPromo()) {
+            return "<div class='col-xs-6 no-padding'><strong>" + flavor + "</strong></div>";
+        } else {
+            return "<div class='col-xs-6 no-padding'><strong>" + flavor + "</strong></div>";
+        }
+    }
+
+    public String valor(Item i) {
+        if (i.getProduct().getPromo()) {
+            return "<div class='text-right'><small><s>" + OUtils.formatarMoeda(getPrice().doubleValue()) + "</s></small> <span class='color-value'>" + OUtils.formatarMoeda(i.getProduct().getPrice().doubleValue()) + "</span></div></div>";
+        } else {
+            return "<div class='col-xs-6 text-right'><span class='color-value'>" + OUtils.formatarMoeda(i.getProduct().getPrice().doubleValue()) + "</span></div>";
+        }
+    }
+
+    public String descricao(Item i) {
+        if (description != null) {
+            System.out.println("siadiashdsuiadhsauidhsauidhaiudhuiasgduisag");
+            return "<div class='col-xs-12 no-padding'><i style='font-size: 12px; color: gray; font-weight: 400; display: inline-table;'>" + description + "</i></div>";
+        } else {
+            return "<div class='col-xs-12 no-padding'><i style='font-size: 12px; color: gray; font-weight: 400; display: inline-table;'>Pizzas</i></div>";
+        }
+    }
+
+    public String getHtmlDescription(Item i) {
+        try {
+            if (flavor != null) {
+                if (description != null) {
                     return "";
                 } else {
                     return "";
@@ -79,12 +122,12 @@ public class FlavorPizza {
             } else {
                 return "";
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return "";
         }
     }
-    
+
     public void setFlavor(String flavor) {
         this.flavor = flavor;
     }
